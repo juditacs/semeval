c223cd66572818ad2b20f0ceb28d8c07c4bbfd32
Gabor Recski <recski@sztaki.hu>
2014-10-16 17:59:12 +0200

    extra env var for hunmorph

--- set_env_nessi6.sh
+++ set_env_nessi6.sh
@@ -1,2 +1,3 @@
 export STANFORD_PARSER=/home/recski/projects/stanford_dp/stanford-parser-full-2014-08-27
 export SEMEVAL_DATA=/mnt/store/home/recski/sts
+export HUNMORPH_DIR=/home/recski/projects/hundisambig_compact
