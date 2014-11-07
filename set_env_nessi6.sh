f1dbe1aa852aed80b9ebcc39f361743415319109
Gabor Recski <recski@sztaki.hu>
2014-11-07 11:08:09 +0100

    added MACHINEPATH

--- set_env_nessi6.sh
+++ set_env_nessi6.sh
@@ -1,3 +1,4 @@
 export STANFORD_PARSER=/home/recski/projects/stanford_dp/stanford-parser-full-2014-08-27
 export SEMEVAL_DATA=/mnt/store/home/recski/sts
 export HUNMORPH_DIR=/home/recski/projects/hundisambig_compact
+export MACHINEPATH=/home/recski/projects/machine
