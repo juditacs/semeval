10440765024651197a0a57832e02159621ac689a
Gabor Recski <recski@mokk.bme.hu>
2015-01-14 23:41:00 +0100

    removed MACHINEPATH and added FOURLANGPATH we should get rid of this file soon 	modified:   set_env_nessi6.sh

--- set_env_nessi6.sh
+++ set_env_nessi6.sh
@@ -1,5 +1,5 @@
 export STANFORD_PARSER=/home/recski/projects/stanford_dp/stanford-parser-full-2014-08-27
 export SEMEVAL_DATA=/mnt/store/home/recski/sts
 export HUNMORPH_DIR=/home/recski/projects/hundisambig_compact
-export MACHINEPATH=/home/recski/projects/machine
 export LSA_DIR=/home/judit/projects/semeval/lsa
+export FOURLANGPATH=/home/recski/projects/4lang
