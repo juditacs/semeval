e9a6fbdaf85ea13f520a31d29152cf051ca86b61
Katalin Pajkossy <pajkossy@info.ilab.sztaki.hu>
2015-11-04 13:58:53 +0100

    modified:   semeval/paraphrases.py 	modified:   semeval/regression.py

--- semeval/regression.py
+++ semeval/regression.py
@@ -1,51 +1,16 @@
 from numpy import linalg
 from sentence import SentencePair
-from collections import defaultdict
 import logging
+import cPickle
+from read_and_enrich import ReadAndEnrich
+from align_and_penalize import AlignAndPenalize
 
-class Regression(object):
+class Featurizer(object):
 
     def __init__(self, conf, reader, aligner):
-        self.conf = conf
         self.reader = reader
         self.aligner = aligner
-        self._feat_order = {}
-        self._feat_i = 0
-
-    def regression(self):
-        logging.info("similarities: {0}".format(
-            self.aligner.similarities.keys()))
-        logging.info('featurizing train...')
-        with open(self.conf.get('regression', 'train')) as f:
-            self.train = self.featurize(f)
-        with open(self.conf.get('regression', 'train_labels')) as f:
-            self.train_labels = self.read_labels(f)
-        self.reader.clear_pairs()
-        logging.info('featurizing test...')
-        with open(self.conf.get('regression', 'test')) as f:
-            self.test = self.featurize(f)
-        logging.info('converting...')
-        self.train_feats = self.convert_to_table(self.train)
-        self.test_feats = self.convert_to_table(self.test)
-        # this created matrices whose rows are sens and columns are sim types
-        logging.info('running regression...')
-        self.model = linalg.lstsq(self.train_feats, self.train_labels)[0]
-        # logging.info('model: {0}'.format(self.model))
-        logging.info('predicting...')
-        self.predicted = self.predict_regression(self.test_feats, 0.3)
-        with open(self.conf.get('regression', 'outfile'), 'w') as f:
-            f.write('\n'.join(str(i) for i in self.predicted) + '\n')
-            # f.write('\n'.join(str(int(i if i < 0.5 else 1.0))
-            #                   for i in self.predicted) + '\n')
-
-    def read_labels(self, stream, true_th=0.5):
-        labels = []
-        for l in stream:
-            f = float(l.strip().split('\t')[-1])
-            if self.conf.getboolean('regression', 'binary_labels'):
-                f = 0 if f < true_th else 1
-            labels.append(f)
-        return labels
+        self.conf = conf
 
     def featurize(self, stream):
         sample = []
@@ -56,6 +21,101 @@ class Regression(object):
             sample.append(pair)
         return sample
 
+class RegressionModel:
+
+    def __init__(self, model_name, train_data, train_labels,
+                 test_data):
+         self.model_name = model_name
+         self.train_data = train_data
+         self.train_labels = train_labels
+         self.test_data = test_data
+
+    def train(self):
+        if self.model_name == 'linalg_lstsq':
+            self.model = linalg.lstsq(self.train_data, self.train_labels)[0]
+    
+    def predict(self):
+        if self.model_name == 'linalg_lstsq':
+            return self.predict_regression(self.test_data)
+     
+    def predict_regression(self, feats, true_th=0.5):
+        scores = []
+        for sample in feats:
+            ans = sum(self.model[i] * x for i, x in enumerate(sample))
+            scores.append(ans)
+        return scores
+        
+
+class Regression(object):
+
+    def __init__(self, conf):
+         self.conf = conf
+         self._feat_order = {}
+         self._feat_i = 0
+
+    def regression(self):
+
+        self.get_regression_model()
+        logging.info('training model...')
+        self.regression_model.train()
+        logging.info('predicting...')
+        predicted = self.regression_model.predict()
+        with open(self.conf.get('regression', 'outfile'), 'w') as f:
+            f.write('\n'.join(str(i) for i in predicted) + '\n')
+        self.dump_if_needed()
+        
+    def get_regression_model(self):
+        
+        model_name = self.conf.get('ml', 'model_name')
+        if self.conf.get('ml', 'load_model') == 'true':
+            logging.info('loading featurized data...')
+            self.regression_model = cPickle.load(open(self.conf.get(
+                'ml', 'load_model_fn')))
+            self.regression_model.model_name = model_name
+
+        else:
+            if self.conf.get('ml', 'load_featurizer') == 'true':
+                logging.info('loading featurizer...')
+                self.featurizer = cPickle.load(open(self.conf.get(
+                    'ml', 'load_featurizer_fn')))
+                                              
+            else:    
+                reader = ReadAndEnrich(self.conf)
+                aligner = AlignAndPenalize(self.conf)
+                self.featurizer = Featurizer(self.conf, reader, aligner) 
+
+            logging.info('featurizing train...')
+            with open(self.conf.get('regression', 'train')) as f:
+                train = self.featurizer.featurize(f)
+            with open(self.conf.get('regression', 'train_labels')) as f:
+                train_labels = self.read_labels(f)
+            self.featurizer.reader.clear_pairs()
+            logging.info('featurizing test...')
+            with open(self.conf.get('regression', 'test')) as f:
+                test = self.featurizer.featurize(f)
+            logging.info('converting...')    
+            train_feats = self.convert_to_table(train)
+            test_feats = self.convert_to_table(test)
+            self.regression_model = RegressionModel(model_name, train_feats, train_labels, test_feats) 
+            # model stores config data so that it is possible to reproduce featurizing
+            self.regression_model.conf = self.conf
+
+    def dump_if_needed(self):
+
+        if self.conf.get('ml', 'dump_model') == 'true':
+            logging.info('dumping featurized data...')
+            with open(self.conf.get('ml', 'dump_model_fn'), 'w') as f:
+                cPickle.dump(self.regression_model, f)
+
+    def read_labels(self, stream, true_th=0.5):
+        labels = []
+        for l in stream:
+            f = float(l.strip().split('\t')[-1])
+            if self.conf.getboolean('regression', 'binary_labels'):
+                f = 0 if f < true_th else 1
+            labels.append(f)
+        return labels
+    
     def convert_to_table(self, sample):
         table = []
         for s in sample:
@@ -71,40 +131,4 @@ class Regression(object):
                     table[-1].append(sc)
                 else:
                     table[-1][self._feat_order[feat]] = sc
-        return table
-
-    def predict_regression(self, feats, true_th=0.5):
-        scores = []
-        for sample in feats:
-            ans = sum(self.model[i] * x for i, x in enumerate(sample))
-            if self.conf.getboolean('regression', 'binary_labels'):
-                f = 0 if ans < true_th else 1
-                scores.append(f)
-            else:
-                scores.append(ans)
-        return scores
-
-    def print_results(self):
-        try:
-            gold_fn = self.conf.get('regression', 'gold')
-            with open(gold_fn) as f:
-                gold = self.read_labels(f)
-        except:
-            gold = None
-            return
-        stat = defaultdict(int)
-        for i, g in enumerate(self.predicted):
-            if g == 1:
-                if gold[i] == 1:
-                    stat['tp'] += 1
-                else:
-                    stat['fp'] += 1
-            else:
-                if gold[i] == 1:
-                    stat['fn'] += 1
-                else:
-                    stat['tn'] += 1
-        prec = stat['tp'] / float(stat['tp'] + stat['fp'])
-        rec = stat['tp'] / float(stat['tp'] + stat['fn'])
-        F = 2 * prec * rec / (prec + rec)
-        print('prec: {0}\nrec: {1}\nF1: {2}'.format(prec, rec, F))
+        return table            
