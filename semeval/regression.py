595b5f2f4fadb57cc19a2c4dbbf666ef4b56af0b
Katalin Pajkossy <pajkossy@info.ilab.sztaki.hu>
2015-12-04 15:14:40 +0100

    modified:   semeval/regression.py framwork for experimenting (parameter ranges to be specified in config fil)

--- semeval/regression.py
+++ semeval/regression.py
@@ -1,4 +1,5 @@
 from numpy import linalg, array
+from itertools import product
 from sklearn import linear_model
 from sklearn import kernel_ridge
 from sklearn import svm
@@ -102,24 +103,62 @@ class Regression(object):
 
     def __init__(self, conf):
          self.conf = conf
-         self.train_fn = conf.get('regression', 'train')
-         self.train_labels_fn = conf.get('regression', 'train_labels')
-         self.test_fn = conf.get('regression', 'test')
-         self.gold_labels_fn = conf.get('regression', 'gold')
-         self.model_name = conf.get('ml', 'model_name')
-         self.load_model = conf.getboolean('ml', 'load_model')
-         self.load_model_fn = conf.get('ml', 'load_model_fn')
-         self.dump_model = conf.getboolean('ml', 'dump_model')
-         self.dump_model_fn = conf.get('ml', 'dump_model_fn')
-         self.feat_select = conf.getboolean('ml', 'feat_select')
-         self.feat_select_thr = conf.get('ml', 'feat_select_thr')
-         self.binary_labels = conf.getboolean('regression', 'binary_labels')
-         self.outfile_fn = conf.get('regression', 'outfile')
          self._feat_order = {}
          self._feat_i = 0
+         for item in conf.items('ml') + conf.items('regression'):
+             self.set_attribute(item[0], item[1])
+         if self.experiment:
+             self.experiment_options = [literal_eval(i[1]) for i
+                                        in conf.items('experiment')]
+         
+    def set_attribute(self, name, value):
+        if name == 'train':
+            self.train_fn = value
+        if name == 'train_labels':
+            self.train_labels_fn = value
+        if name == 'test':
+            self.test_fn = value
+        if name == 'gold':
+            self.gold_labels_fn = value
+        if name == 'model_name':
+            self.model_name = value
+        if name == 'load_model':
+            self.load_model = value
+        if name == 'load_model_fn':
+            self.load_model_fn = value 
+        if name == 'dump_model':
+            self.dump_model = value
+        if name == 'dump_model_fn':
+            self.dump_model_fn = value
+        if name == 'feat_select':
+            self.feat_select = value
+        if name == 'feat_select_thr':
+            self.feat_select_thr = value
+        if name == 'binary_labels':
+            self.binary_labels = value
+        if name == 'outfile':
+            self.outfile_fn = value
+        if name == 'experiment': 
+            self.experiment = value
 
 
-    def regression(self, dump_predicted_labels=True):
+    def set_exp_params(self, list_):
+        for d in list_:
+            for k in d:
+                v = d[k]
+                self.set_attribute(k, v)
+
+    def regression(self):
+        if self.experiment == 'true':
+            for exp_param in product(*self.experiment_options):
+                logging.info('experimenting with option {}'.format(repr(exp_param)))
+                self.set_exp_params(exp_param)
+                self.regression_item(dump_predicted_labels=False)
+        else:
+            self.regression_item()
+
+
+    def regression_item(self, dump_predicted_labels=True):
         # featurize /load existing model (with its featurized training and test sets)
         self.get_training_setup()
         logging.info('training model...')
@@ -135,7 +174,7 @@ class Regression(object):
 
     def get_training_setup(self):
 
-        if self.load_model:
+        if self.load_model == 'true':
             logging.info('loading featurized data...')
             self.regression_model = cPickle.load(open(self.load_model_fn))
             self.regression_model.model_name = self.model_name
@@ -162,7 +201,7 @@ class Regression(object):
             # model stores config data so that it is possible to reproduce featurizing
             self.regression_model.conf = self.conf
             self.regression_model.feats = self._feat_order
-        if self.feat_select:
+        if self.feat_select == 'true':
             self.regression_model.feat_select_thr =\
                     float(self.feat_select_thr)
         else:
@@ -170,7 +209,7 @@ class Regression(object):
 
     def dump_if_needed(self, predicted, dump_predicted_labels):
 
-        if self.dump_model:
+        if self.dump_model == 'true':
             logging.info('dumping featurized data...')
             with open(self.dump_model_fn, 'w') as f:
                 cPickle.dump(self.regression_model, f)
@@ -183,7 +222,7 @@ class Regression(object):
         labels = []
         for l in stream:
             f = float(l.strip().split('\t')[-1])
-            if self.binary_labels:
+            if self.binary_labels == 'true':
                 f = 0 if f < true_th else 1
             labels.append(f)
         return array(labels)
